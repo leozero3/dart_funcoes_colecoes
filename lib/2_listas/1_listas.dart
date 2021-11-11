570039fca2f0cd6fe06ffb2a38ac147d48e4fe02
leo <realleo2009@hotmail.com>
2021-11-10 22:40:55 -0300

    funcoes e colecoes

--- lib/2_listas/1_listas.dart
+++ lib/2_listas/1_listas.dart
@@ -1,3 +1,6 @@
+// ignore: file_names
+// ignore_for_file: file_names
+
 void main() {
   var numeros = List.generate(10, (index) => index);
   numeros.forEach(print);
@@ -33,4 +36,65 @@ void main() {
 
   //sort
   var listaParaOrdenacao = [75, 248, 369, 2, 79, 7, 3, 8];
+  listaParaOrdenacao.sort();
+  print(listaParaOrdenacao);
+
+  var listaNomesOrdenacao = ['leo', 'amanda', 'isaac'];
+  listaNomesOrdenacao.sort();
+  print(listaNomesOrdenacao);
+
+  ///
+  var listapacientes = [
+    'Rodrigo Rahman|35|desenvolvedor|SP',
+    'Manoel Silva|12|estudante|MG',
+    'Joaquim Rahman|18|estudante|SP',
+    'Fernando Verne|35|estudante|MG',
+    'Gustavo Silva|40|desenvolvedor|MG',
+    'Sandra Silva|40|Desenvolvedor|MG',
+    'Regina Verne|35|dentista|MG',
+    'João Rahman|55|jornalista|SP',
+  ];
+
+  listapacientes.sort((paciente1, paciente2) {
+    final pacientesdados1 = paciente1.split('|');
+    final pacientesdados2 = paciente2.split('|');
+
+    final idadePaciente1 = int.parse(pacientesdados1[1]);
+    final idadePaciente2 = int.parse(pacientesdados2[1]);
+
+    if (idadePaciente1 > idadePaciente2) {
+      return 1;
+    } else if (idadePaciente2 == 0) {
+      return 0;
+    } else {
+      return -1;
+    }
+  });
+
+  print(listapacientes);
+
+  //CompareTo
+
+  var listapacientes2 = [
+    'Rodrigo Rahman|35|',
+    'Manoel Silva|12|',
+    'Joaquim Rahman|18|',
+    'Fernando Verne|35|',
+    'Gustavo Silva|40|',
+    'Sandra Silva|40|',
+    'Regina Verne|35|',
+    'João Rahman|55|',
+  ];
+
+  listapacientes2.sort((paciente1, paciente2) {
+    final pacientesdados1 = paciente1.split('|');
+    final pacientesdados2 = paciente2.split('|');
+
+    final idadePaciente1 = int.parse(pacientesdados1[1]);
+    final idadePaciente2 = int.parse(pacientesdados2[1]);
+
+    return idadePaciente1.compareTo(idadePaciente2);
+  });
+
+  print(listapacientes2);
 }
